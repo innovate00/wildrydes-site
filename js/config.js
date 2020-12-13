1723693454e6645d246d006480048e4a5c397cfc
innovate00 <innovate00@naver.com>
2020-12-13 14:28:59 +0900

    config.js 수정

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_ZovI8Bgwl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4j9ekr2vq9s7nt0q76pq967gsv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
